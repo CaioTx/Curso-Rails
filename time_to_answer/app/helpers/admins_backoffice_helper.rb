88a34ca136f4c097b8573211bf3d6f4b8487fe37
CaioTx <caioaraujo.teixeira@gmail.com>
2022-11-18 16:26:07 -0300

    daily commit

--- time_to_answer/app/helpers/admins_backoffice_helper.rb
+++ time_to_answer/app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+  def translate_attribute( object = nil, method = nil)
+    if object && method
+    object.model.human_attribute_name(:description)
+    else
+      "informe os parametros corretamente!"
+   end
+  end
 end
